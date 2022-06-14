d5602bf167f64ac670745e3822e346215354aa81
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-14 11:28:12 +0530

    responses for habitat page populated

--- routes/habitats.js
+++ routes/habitats.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
+const pool = require("../database");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.end("Your now on habitats page");
+router.get("/", (request, response) => {
+  pool.query("SELECT * FROM habitats", (err, res) => {
+    if (err) console.log(err);
+    response.json(res.rows);
+  });
 });
 
 module.exports = router;
