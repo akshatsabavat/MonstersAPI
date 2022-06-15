10acf3acb550fe4fdbd1d97f64c7d84bc7cb45ef
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-06-15 20:44:19 +0530

    API completed

--- routes/habitats.js
+++ routes/habitats.js
@@ -1,9 +1,17 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const pool = require("../database");
 const router = Router();
 
 router.get("/", (request, response) => {
   pool.query("SELECT * FROM habitats", (err, res) => {
+    if (err) response.end(err.message);
+    response.json(res.rows);
+  });
+});
+
+router.get("/:id", (request, response) => {
+  const { id } = request.params;
+  pool.query("SELECT * FROM habitats WHERE id = ($1)", [id], (err, res) => {
     if (err) response.end(err.message);
     response.json(res.rows);
   });
--- routes/lives.js
+++ routes/lives.js
@@ -1,8 +1,22 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
+const pool = require("../database");
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.end("Your now on lives page");
+router.get("/", (request, response) => {
+  pool.query("SELECT * FROM lives", (err, res) => {
+    if (err) response.end(err.message);
+    response.json(res.rows);
+  });
+});
+
+router.get("/conditions", (request, response) => {
+  pool.query(
+    "SELECT * FROM habitats JOIN lives ON habitats.name = lives.habitat",
+    (err, req) => {
+      if (err) response.end(err.message);
+      response.json(req.rows);
+    }
+  );
 });
 
 module.exports = router;
